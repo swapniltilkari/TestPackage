ebbebdb78c71a43dc2a39a0cfefdf38e67aaa8e8
Swapnil Tilkari <Swapnil.Tilkari@Amway.com>
2022-03-21 13:35:13 +0530

    My First Swift Package Demo 2

--- TestPackage/Package.swift
+++ TestPackage/Package.swift
@@ -5,6 +5,10 @@ import PackageDescription
 
 let package = Package(
     name: "TestPackage",
+    platforms: [
+      // Only add support for iOS 13 and up.
+      .iOS(.v13)
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
